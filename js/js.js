4c32f8ecd154c2527d50aa66205243f91dc8aba8
reyLipa <curso.lipa@gmail.com>
2024-07-18 01:16:40 -0300

    repaso git

---
+++ js/js.js
@@ -0,0 +1,7 @@
+let cf, pv,cv, pe
+cf=Number(prompt("ingrese los costos fijos"))
+pv=Number(prompt("ingrese el precio de venta"))
+cv=Number(prompt("ingrese el costo variable"))
+
+pe=cf/(pv-cv)
+console.log("el pe es")
